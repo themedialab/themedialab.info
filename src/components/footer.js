ef76fea0f83cd11da50d1f52576454569b39e926
themedialab <72581783+themedialab@users.noreply.github.com>
2020-10-09 01:59:33 +0200

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>A GatsbyJS Starter for Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Stackrole.com</Link></p>
+      <p>Et lynraskt nettsted og blogg, bygget med <Link to="/">GatsbyJS</Link></p>
     </div>
   </footer>
 )
