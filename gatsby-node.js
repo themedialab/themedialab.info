2ffab913144ef02d940f8c8ea1d5f42acd00aee8
themedialab <72581783+themedialab@users.noreply.github.com>
2020-10-09 03:34:03 +0200

    Update blog path in gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -66,7 +66,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 
   Array.from({ length: numPages }).forEach((_, i) => {
     createPage({
-      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+      path: i === 0 ? `/blogg` : `/blogg/${i + 1}`,
       component: blogList,
       context: {
         limit: postsPerPage,
