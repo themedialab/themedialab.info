00d7e9935d769300707aec231d6c0436cafd5eca
themedialab <72581783+themedialab@users.noreply.github.com>
2020-10-09 17:44:52 +0200

    Updated the PWA manifest in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,8 +79,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
+        name: `The Media Lab`,
+        short_name: `TheMediaLab`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
